62561c338a98dae421a3fe0c18a2a9543be41765
Ricardo Santos <139587525+TUR14CUS@users.noreply.github.com>
2024-02-01 00:40:05 +0000

    Add files via upload

---
+++ README.md
@@ -0,0 +1,70 @@
+# Lusitan Blockchain
+
+Lusitan Blockchain is a decentralized cryptocurrency system implemented in Python using Flask and Django.
+
+## Table of Contents
+
+- [Features](#features)
+- [Prerequisites](#prerequisites)
+- [Getting Started](#getting-started)
+- [API Endpoints](#api-endpoints)
+- [Contributing](#contributing)
+- [License](#license)
+
+## Features
+
+- Proof-of-work based consensus mechanism
+- Decentralized blockchain network
+- Mining and transaction functionalities
+- Secure digital signatures for transactions
+- Flask and Django implementations for nodes
+
+## Prerequisites
+
+- Python 3.x
+- Flask
+- Django
+- Postman (for testing)
+
+## Getting Started
+
+1. Clone the repository:
+
+```bash
+git clone https://github.com/TUR14CUS/lusitan-blockchain.git
+cd lusitan-blockchain
+```
+
+2. Run the nodes:
+
+- For Node 1:
+
+```bash
+python lusitan_node_5000.py
+```
+
+- For Node 2:
+
+```bash
+python lusitan_node_5001.py
+```
+
+...
+
+## API Endpoints
+
+- `/mine_block`: Mine a new block.
+- `/get_chain`: Retrieve the full blockchain.
+- `/is_valid`: Check if the blockchain is valid.
+- `/add_transaction`: Add a new transaction to the blockchain.
+- `/connect_node`: Connect a new node to the blockchain network.
+- `/replace_chain`: Replace the chain with the longest one in the network.
+
+## Contributing
+
+Contributions are welcome! Please follow the [CONTRIBUTING.md](CONTRIBUTING.md) guidelines.
+
+## License
+
+This project is licensed under the MIT License - see the [LICENSE](LICENSE) file for details.
+```
---
+++ lusitan_node_5002.py
@@ -0,0 +1,106 @@
+import datetime
+import hashlib
+import json
+from django.contrib.sites import requests
+from flask import Flask, jsonify, request
+from flask_restful import Api, Resource
+from uuid import uuid4
+from urllib.parse import urlparse
+
+app = Flask(__name__)
+api = Api(app)
+
+class Blockchain(Resource):
+    def __init__(self):
+        self.chain = []
+        self.transactions = []
+        self.nodes = set()
+
+    def create_block(self, proof, previous_hash):
+        block = {
+            'index': len(self.chain) + 1,
+            'timestamp': str(datetime.datetime.now()),
+            'proof': proof,
+            'previous_hash': previous_hash,
+            'transactions': self.transactions
+        }
+        self.transactions = []
+        self.chain.append(block)
+        return block
+
+    def get_previous_block(self):
+        return self.chain[-1]
+
+    def proof_of_work(self, previous_proof):
+        new_proof = 1
+        check_proof = False
+        while not check_proof:
+            hash_operation = hashlib.sha3_256((str(new_proof**2 - previous_proof**2)).encode()).hexdigest()
+            if hash_operation[:4] == '0000':
+                check_proof = True
+            else:
+                new_proof += 1
+        return new_proof
+
+    def hash(self, block):
+        encoded_block = json.dumps(block, sort_keys=True).encode()
+        return hashlib.sha3_256(encoded_block).hexdigest()
+
+    def is_chain_valid(self, chain):
+        previous_block = chain[0]
+        for block in chain[1:]:
+            if block['previous_hash'] != self.hash(previous_block):
+                return False
+            previous_proof, proof = previous_block['proof'], block['proof']
+            hash_operation = hashlib.sha3_256((str(proof**2 - previous_proof**2)).encode()).hexdigest()
+            if hash_operation[:4] != '0000':
+                return False
+            previous_block = block
+        return True
+
+    def add_transaction(self, sender, receiver, amount, signature):
+        transaction = {
+            'sender': sender,
+            'receiver': receiver,
+            'amount': amount,
+            'signature': signature
+        }
+        if not self.valid_transaction(transaction):
+            return "Invalid transaction", 400
+        self.transactions.append(transaction)
+        return self.get_previous_block()['index'] + 1
+
+    def mine_block(self):
+        previous_block = self.get_previous_block()
+        previous_proof = previous_block['proof']
+        proof = self.proof_of_work(previous_proof)
+        previous_hash = self.hash(previous_block)
+        self.add_transaction(sender=node_address, receiver='Hadelin', amount=1, signature="digital_signature")
+        block = self.create_block(proof, previous_hash)
+        return block
+
+    def add_node(self, address):
+        parsed_url = urlparse(address)
+        self.nodes.add(parsed_url.netloc)
+
+    def consensus(self):
+        network = self.nodes
+        longest_chain = None
+        max_length = len(self.chain)
+        for node in network:
+            response = requests.get(f'http://{node}/blockchain')
+            if response.status_code == 200:
+                length, chain = response.json()['length'], response.json()['chain']
+                if length > max_length and self.is_chain_valid(chain):
+                    max_length, longest_chain = length, chain
+        if longest_chain:
+            self.chain = longest_chain
+            return True
+        return False
+
+api.add_resource(Blockchain, '/blockchain')
+
+if __name__ == '__main__':
+    node_address = str(uuid4()).replace('-', '')
+    blockchain = Blockchain()
+    app.run(host='0.0.0.0', port=5002)
